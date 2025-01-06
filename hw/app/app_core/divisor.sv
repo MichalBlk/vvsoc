1130c0422f5df89ead12891e2db5e471f819f965
Michał Błaszczyk <64263550+MichalBlk@users.noreply.github.com>
2025-01-06 22:32:10 +0100

    [APP-CORE] Implement correct semantics for division by zero and division overflow (#10)

--- hw/app/app_core/divisor.sv
+++ hw/app/app_core/divisor.sv
@@ -4,6 +4,10 @@
 
 module divisor
   import isa_pkg::*;
+#(
+  localparam ALL = {XLEN{1'b1}},
+  localparam MAXPW = 1 << (XLEN - 1)
+)
 (
   input  logic [XLEN - 1:0]      ac_src1,
   input  logic [XLEN - 1:0]      ac_src2,
@@ -16,6 +20,8 @@ module divisor
   logic [XLEN - 1:0]        q_u;
   logic [XLEN - 1:0]        r_s;
   logic [XLEN - 1:0]        r_u;
+  logic                     zero;
+  logic                     ovf;
 
   assign src1_s = ac_src1;
   assign src2_s = ac_src2;
@@ -25,12 +31,39 @@ module divisor
   assign r_s = src1_s % src2_s;
   assign r_u = ac_src1 % ac_src2;
 
+  assign zero = !ac_src2;
+  assign ovf  = ac_src1 == MAXPW && ac_src2 == ALL;
+
   always_comb
     case (ac_funct3)
-      FUNCT3_DIV:  ac_res = q_s;
-      FUNCT3_DIVU: ac_res = q_u;
-      FUNCT3_REM:  ac_res = r_s;
-      FUNCT3_REMU: ac_res = r_u;
-      default:     ac_res = 'bx;
+      FUNCT3_DIV:
+        if (zero)
+          ac_res = ALL;
+        else if (ovf)
+          ac_res = MAXPW;
+        else
+          ac_res = q_s;
+
+      FUNCT3_DIVU:
+        if (zero)
+          ac_res = ALL;
+        else
+          ac_res = q_u;
+
+      FUNCT3_REM:
+        if (zero)
+          ac_res = ac_src1;
+        else if (ovf)
+          ac_res = 0;
+        else
+          ac_res = r_s;
+
+      FUNCT3_REMU:
+        if (zero)
+          ac_res = ac_src1;
+        else
+          ac_res = r_u;
+
+      default: ac_res = 'bx;
     endcase
 endmodule
