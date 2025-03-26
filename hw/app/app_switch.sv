1baa5165aacce9242df0edc004b71a874b4799c8
MichalBlk <michal.blaszczyk09@gmail.com>
2025-03-26 12:39:23 +0100

    Cleanup

--- hw/app/app_switch.sv
+++ hw/app/app_switch.sv
@@ -79,8 +79,10 @@ module app_switch
     endcase
   end
 
+/*
   always_ff @(posedge clk)
     if ((ac_ren || ac_wen) && dev != DEV_VCD && dev != DEV_VGD && dev != DEV_DBGC &&
       dev != DEV_CLINT && dev != DEV_MMEM)
       $display("[ASW] Unknown device %h!", dev);
+*/
 endmodule
