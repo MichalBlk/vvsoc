ca3691542764a70fd73010d2e5f6669e1c5b4356
MichalBlk <michal.blaszczyk09@gmail.com>
2025-05-05 11:04:37 +0200

    Simple synchronizers

---
+++ hw/common/flip_flop_sync.sv
@@ -0,0 +1,32 @@
+`default_nettype none
+
+module flip_flop_sync #(
+  parameter WIDTH = 32,
+  parameter CNT   = 2
+)(
+  input  logic               clk,
+  input  logic               nrst,
+
+  input  logic [WIDTH - 1:0] src,
+  output logic [WIDTH - 1:0] res
+);
+  (* ASYNC_REG = "TRUE" *)
+  logic [WIDTH - 1:0] data [CNT - 1:0], data_r [CNT - 1:0];
+
+  always_comb begin
+    data[0] = src;
+
+    for (int i = 1; i < CNT; i++)
+      data[i] = data_r[i - 1];
+  end
+
+  always @(posedge clk, negedge nrst)
+    if (!nrst)
+      for (int i = 0; i < CNT; i++)
+        data_r[i] <= 0;
+    else
+      for (int i = 0; i < CNT; i++)
+        data_r[i] <= data[i];
+
+  assign res = data_r[CNT - 1];
+endmodule
