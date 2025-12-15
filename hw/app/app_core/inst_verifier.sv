6525bfca65bb7ea1dd90faea67988d574e9cc00a
MichalBlk <michal.blaszczyk09@gmail.com>
2025-12-15 20:50:23 +0100

    Remove consecutive instruction opt and cleanup

--- hw/app/app_core/inst_verifier.sv
+++ hw/app/app_core/inst_verifier.sv
@@ -10,6 +10,7 @@ module inst_verifier
   input  logic [FUNCT5LEN - 1:0]  ac_funct5,
   input  logic [FUNCT7LEN - 1:0]  ac_funct7,
   input  logic [FUNCT12LEN - 1:0] ac_funct12,
+  output logic                    ac_nop,
   output logic                    ac_mul,
   output logic                    ac_div,
   output logic                    ac_ecall,
@@ -24,10 +25,12 @@ module inst_verifier
   logic store;
   logic op_imm;
   logic op;
+  logic fence;
   logic amo_lr;
   logic amo_sc;
   logic amo_rmw;
   logic csr;
+  logic wfi;
 
   assign branch = ac_opcode == OPCODE_BRANCH &&
     (ac_funct3 == FUNCT3_BEQ || ac_funct3 == FUNCT3_BNE || ac_funct3 == FUNCT3_BLT ||
@@ -61,6 +64,9 @@ module inst_verifier
     (ac_funct3 == FUNCT3_OR && ac_funct7 == FUNCT7_OR) ||
     (ac_funct3 == FUNCT3_AND && ac_funct7 == FUNCT7_AND);
 
+  assign fence = ac_opcode == OPCODE_MISC_MEM &&
+    (ac_funct3 == FUNCT3_FENCE || ac_funct3 == FUNCT3_FENCEI);
+
   assign ac_mul = ac_opcode == OPCODE_OP && (ac_funct3 == FUNCT3_MUL || ac_funct3 == FUNCT3_MULH ||
     ac_funct3 == FUNCT3_MULHSU || ac_funct3 == FUNCT3_MULHU) && ac_funct7 == FUNCT7_MULDIV;
   assign ac_div = ac_opcode == OPCODE_OP && (ac_funct3 == FUNCT3_DIV || ac_funct3 == FUNCT3_DIVU ||
@@ -85,6 +91,8 @@ module inst_verifier
   assign ac_sret = ac_opcode == OPCODE_SYSTEM && ac_funct3 == FUNCT3_PRIV &&
     ac_funct12 == FUNCT12_SRET;
 
+  assign wfi = ac_opcode == OPCODE_SYSTEM && ac_funct3 == FUNCT3_PRIV && ac_funct12 == FUNCT12_WFI;
+
   assign ac_sfence_vma = ac_opcode == OPCODE_SYSTEM && ac_funct3 == FUNCT3_PRIV &&
     ac_funct7 == FUNCT7_SFENCE_VMA;
 
@@ -92,7 +100,9 @@ module inst_verifier
     (ac_funct3 == FUNCT3_CSRRW || ac_funct3 == FUNCT3_CSRRS || ac_funct3 == FUNCT3_CSRRC ||
      ac_funct3 == FUNCT3_CSRRWI || ac_funct3 == FUNCT3_CSRRSI || ac_funct3 == FUNCT3_CSRRCI);
 
+  assign ac_nop = fence | wfi;
+
   assign ac_valid = ac_opcode == OPCODE_LUI || ac_opcode == OPCODE_AUIPC || ac_opcode == OPCODE_JAL ||
-    ac_opcode == OPCODE_JALR || branch || load || store || op_imm || op || ac_mul || ac_div ||
+    ac_opcode == OPCODE_JALR || branch || load || store || op_imm || op || ac_nop || ac_mul || ac_div ||
     amo_lr || amo_sc || amo_rmw || csr || ac_ecall || ac_ebreak || ac_mret || ac_sret || ac_sfence_vma;
 endmodule
