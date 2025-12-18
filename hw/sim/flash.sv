0c9eb0bc7bf995849f0d7d044a919cbb4b48b8d5
MichalBlk <michal.blaszczyk09@gmail.com>
2025-12-18 19:55:50 +0100

    Use flash in simulation

--- hw/sim/flash.sv
+++ hw/sim/flash.sv
@@ -17,9 +17,27 @@ module flash
   output logic [XLEN - 1:0]       asw_rdata,
   output logic                    asw_stall
 );
+  logic [BLEN - 1:0] mem [FLSZ - 1:0];
+
+  initial begin
+    $display("[FL] Loading OpenSBI...");
+    $readmemh("opensbi.mif", mem, FL_OPENSBI_OFF);
+
+    $display("[FL] Loading dtb...");
+    $readmemh("vrvsoc.mif", mem, FL_DTB_OFF);
+
+    $display("[FL] Loading kernel...");
+    $readmemh("kernel.mif", mem, FL_KERNEL_OFF);
+
+    $display("[FL] Loading initrd...");
+    $readmemh("initrd.mif", mem, FL_INITRD_OFF);
+
+    $display("[FL] Images loaded successfully");
+  end
+
   /*
    * Application switch signals
    */
-  assign asw_rdata = 'bx;
+  assign asw_rdata = mem[asw_addr];
   assign asw_stall = 0;
 endmodule
